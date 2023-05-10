c7849d90d8c7764b7b9760fc270f9577bfe92fe4
Ayushgupta26 <ayushgupta33@deloitte.com>
2023-05-10 21:46:12 +0530

    myprofile

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pentiah'`, () => {
+  it(`should have as title 'myProfile'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pentiah');
+    expect(app.title).toEqual('myProfile');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pentiah app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('myProfile app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { FeatureComponent } from './feature/feature.component';
-import { HistoryComponent } from './history/history.component';
-import { RouterModule } from '@angular/router';
-import { SafePipe } from './pipe/safe-pipe.pipe';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent,
-    FeatureComponent,
-    HistoryComponent,
-    SafePipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      {path :'', component: HomeComponent},
-      {path :'contact', component: ContactComponent},  
-      {path :'about', component: AboutComponent}, 
-      {path :'history', component: HistoryComponent}, 
-      {path :'feature', component: FeatureComponent},
-      {path :'**', component: HomeComponent}
-    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+   app = initializeApp(environment.firebase);
+  analytics = getAnalytics(this.app);
+}
